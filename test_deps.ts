83efb168483248782dfcbd777937fa1229031db2
Koji <kk2796@nyu.edu>
2020-08-13 06:35:46 -0400

    modify export for github actions workflow

--- test_deps.ts
+++ test_deps.ts
@@ -1,3 +1,3 @@
 // test dependencies in here
-export { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+export { assertEquals } from "https://deno.land/std@0.64.0/testing/asserts.ts";
 export { runTests } from "./mod_test.ts";
